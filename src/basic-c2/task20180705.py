9f3472814a261dae17092352a19b75723d8a2bf3
n18019 <n18019@std.it-college.ac.jp>
2018-07-05 18:10:37 +0900

    0705課題

---
+++ src/basic-c2/task20180705.py
@@ -0,0 +1,14 @@
+# 買ったもの
+biru = 200*2
+fod = 100
+yaki = (100*2)*0.8
+
+# ポイント引きの値段
+waribiki = 150
+# 買ったも物の合計金額
+goukei = biru + fod + yaki
+
+# 買った合計金額からポイント分を引いた数
+total = goukei - waribiki
+
+print(total)
