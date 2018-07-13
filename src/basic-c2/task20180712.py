608bb1b9f5768d3f6f0a6bc613556171fae5fba3
n18019 <n18019@std.it-college.ac.jp>
2018-07-13 12:46:59 +0900

    0712課題

---
+++ src/basic-c2/task20180712.py
@@ -0,0 +1,7 @@
+print('  |  1  2  3  4  5  6  7  8  9')
+print('--+---------------------------')
+for a in range(1, 10):
+    print(' {}|'.format(a), end='')
+    for b in range(1, 10):
+        print('{:3}'.format(a * b), end='')
+    print('')
