b1a575c055bf1432df94f1045505d7aabeb0d79f
n18019 <n18019@std.it-college.ac.jp>
2018-07-13 12:51:55 +0900

    0712までの授業内容と課題

---
+++ src/basic-c2/if-cafe.py
@@ -0,0 +1,5 @@
+kion = int(input("今日の気温は？"))
+if kion >= 25:
+    print("氷水をだす")
+else:
+    print("熱いお茶をだす")
---
+++ src/basic-c2/kyuryo.py
@@ -0,0 +1,20 @@
+# 時給計算プログラム
+
+# 時給の入力--(1)
+user = input("時給はいくらですか？")
+jikyu = int(user)
+
+# 時間の入力
+user = input("何時間働きましたか？")
+jikan = float(user)
+
+#  計算--(2)
+kyurou = jikyu * jikan
+
+# 結果を表示--(3)
+fmt = """
+時給{0}円で、{1}時間働いたので、、、
+時給は、{2}円です。
+"""
+desc = fmt.format(jikyu, jikan, kyurou)
+print(desc)
