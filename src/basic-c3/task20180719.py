4068da88d05459d68119cedddbb4a41ded374232
n18019 <n18019@std.it-college.ac.jp>
2018-07-19 16:48:38 +0900

    0719課題

--- src/basic-c3/task20180719.py
+++ src/basic-c3/task20180719.py
@@ -6,6 +6,67 @@ arithmetic = {
         '/': 3,
         }
 
+# 計算方法を関数に
+def mul(a, b):
+    '''
+    掛け算を行う
+
+    Parameters
+    ----------
+    a: float
+    b: float
+
+    Returns
+    -------
+    aとbをかけた値
+    '''
+    return a * b
+
+def sub(a, b):
+    '''
+    引き算を行う
+
+    Parameters
+    ----------
+    a: float
+    b: float
+
+    Returns
+    -------
+    aとbを引いた値
+    '''
+    return a - b
+
+def add(a, b):
+    '''
+    足し算を行う
+
+    Parameters
+    ----------
+    a: float
+    b: float
+
+    Returns
+    -------
+    aとbを足した値
+    '''
+    return a + b
+
+def div(a, b):
+    '''
+    割り算を行う
+
+    Parameters
+    ----------
+    a: float
+    b: float
+
+    Returns
+    -------
+    aとbを割った値
+    '''
+    return a / b
+
 print('四則計算プログラムです')
 while True:
     try:
@@ -32,15 +93,15 @@ while True:
     else:
         print('エラーです。再度入力してください')
 
-# arithmetic_wordが文字列としてしか認識されないためifで条件を指定し計算
+# 入力された値を作った関数で計算
 if arithmetic_word == '+':
-    result = user_word + user_word2
+    result = add(user_word, user_word2)
 elif arithmetic_word == '-':
-    result = user_word - user_word2
+    result = sub(user_word, user_word2)
 elif arithmetic_word == '*':
-    result = user_word * user_word2
+    result = mul(user_word, user_word2)
 elif arithmetic_word == '/':
-    result = user_word / user_word2
+    result = div(user_word, user_word2)
 
 # 小数点第３位まで丸める
 result = round(result, 3)
